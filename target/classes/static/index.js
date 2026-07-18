d705ae03cce4e2ada4a5d25fb53e7299e7a86858
VIKRANTH REDDY <kvikranthreddi@gmail.com>
2026-07-18 12:08:59 +0530

    initial comment

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,4 +1,69 @@
+// Array of Users
 
-// alert("Hello guysss");
+var users = [
 
-console.log("hello from JS!")
+    {
+
+        name: "John Doe",
+
+        gender: "Male",
+
+        img: "john.png"
+
+    },
+
+    {
+
+        name: "Jane Doe",
+
+        gender: "Female",
+
+        img: "jane.png"
+
+    }
+
+];
+
+// Current User
+
+var id = 0;
+
+// Toggle User
+
+function toggleUser(){
+
+    id = (id + 1) % 2;
+
+    var userImage = document.getElementById("user-image");
+
+    var userName = document.getElementById("user-name");
+
+    var userGender = document.getElementById("user-gender");
+
+    userImage.src = users[id].img;
+
+    userName.innerHTML = users[id].name;
+
+    userGender.innerHTML = users[id].gender;
+
+}
+
+// Random User
+
+function randomUser(){
+
+    id = Math.floor(Math.random() * users.length);
+
+    var userImage = document.getElementById("user-image");
+
+    var userName = document.getElementById("user-name");
+
+    var userGender = document.getElementById("user-gender");
+
+    userImage.src = users[id].img;
+
+    userName.innerHTML = users[id].name;
+
+    userGender.innerHTML = users[id].gender;
+
+}
